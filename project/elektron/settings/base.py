07743e4b13e8c990e8bfaa497cf01579fbdfe85e
Edward Bridges <github@eqbridges.com>
2018-12-10 18:36:43 -0500

    wire up a context processor to render values into templates

--- project/elektron/settings/base.py
+++ project/elektron/settings/base.py
@@ -90,6 +90,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'elektron.context_processors.selected_settings'
             ],
         },
     },
