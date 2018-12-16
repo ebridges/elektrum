d79740f21528a6b58ccdabc78bc6ecdc38781d20
Edward Bridges <github@eqbridges.com>
2018-12-16 06:59:46 -0500

    add tests for login, and grant access to `testserver` in development

--- project/users/tests.py
+++ project/users/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.test import Client
+from .models import CustomUser
 
 # Create your tests here.
+class SimpleTest(TestCase):
+  def setUp(self):
+    user = CustomUser.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
+  def test_login(self):
+    c = Client()
+    result = c.login(email='temporary@gmail.com', password='temporary')
+    self.assertTrue(result)
+
+  def test_login_fail(self):
+    c = Client()
+    result = c.login(email='temporary@gmail.com', password='xxxxxx')
+    self.assertFalse(result)
+
+  def test_login_via_post(self):
+    c = Client()
+    response = c.post('/account/login/', {'login': 'temporary@gmail.com', 'password': 'temporary'})
+    print(response)
+    self.assertEqual(response.status_code, 302)
+    user = CustomUser.objects.get(username='temporary')
+    self.assertEqual(response.context['user'].email, user.email)
+
+  def test_csrf_failure(self):
+    c = Client(enforce_csrf_checks=True)
+    response = c.post('/account/login/', {'login': 'temporary@gmail.com', 'password': 'temporary'})
+    self.assertEqual(response.status_code, 403)
+
