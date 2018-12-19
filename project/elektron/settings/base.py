967210c1746244486e8978f3b5d1fd1d2ebc7a8e
Edward Bridges <github@eqbridges.com>
2018-12-19 05:41:28 -0500

    configure application db access from env vars

--- project/elektron/settings/base.py
+++ project/elektron/settings/base.py
@@ -105,10 +105,20 @@ WSGI_APPLICATION = 'elektron.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('db_name'),
+        'USER': os.getenv('db_username'),
+        'PASSWORD': os.getenv('db_password'),
+        'HOST': os.getenv('db_hostname'),
+        'PORT': os.getenv('db_port_num'),
     }
 }
 
