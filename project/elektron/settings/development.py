05efe4c9b7631717eeec07647167306637277245
Edward Q. Bridges <ebridges@roja.cc>
2018-12-19 05:37:32 -0500

    Revert "configure development db access to pg"
    This reverts commit 66ef2a3fb50a7c997975234a218ae000f9fe6a95.

--- project/elektron/settings/development.py
+++ project/elektron/settings/development.py
@@ -1,13 +1,3 @@
 from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['testserver', '127.0.0.1', 'localhost']
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '5432',
-    }
-}
