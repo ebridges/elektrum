42f99acfd578fedfb52841aecad5b06fba7349df
Edward Bridges <github@eqbridges.com>
2018-12-20 06:30:53 -0500

    pass ELEKTRON_ENV into playbook to locate correct env file

--- network/run.sh
+++ network/run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+if [ -z "${ELEKTRON_ENV}" ];
+then
+  ELEKTRON_ENV=staging
+fi
+
 ansible-playbook \
   --inventory hosts.ini \
   --vault-password-file ./vault-password.txt \
+  --extra-vars="elektron_env=${ELEKTRON_ENV}" \
   $@ \
   site.yml
