a4c410cfb51ed475efe97129ce7e45d75a85e534
Edward Bridges <github@eqbridges.com>
2019-01-04 20:31:25 -0500

    update deploy script to handle the proxy as well as app image, generate a config for a given env, run tests in a dev env, and to integrate with ecr for pushing images

--- app.sh
+++ app.sh
@@ -1,17 +1,6 @@
 #!/usr/local/bin/bash
 
 CMD=$1
-ELEKTRON_ENV=$2
-
-if [ -z "${ELEKTRON_ENV}" ];
-then
-    # default environment
-    ELEKTRON_ENV=staging
-fi
-
-source "etc/${ELEKTRON_ENV}.env"
-
-ENV_NAME="${service_name}-${ELEKTRON_ENV}"
 
 if [ -z "${CMD}" ];
 then
@@ -19,8 +8,30 @@ then
     exit 0
 fi
 
+if [ -z "${ELEKTRON_ENV}" ];
+then
+    # default environment
+    ELEKTRON_ENV=staging
+fi
+
+if [ ! -f "./etc/${ELEKTRON_ENV}.env" ];
+then
+    echo "Generating configuration for ${ELEKTRON_ENV}"
+    pushd network
+    ./run.sh --tags=common,cfg
+    popd
+else
+    echo "Configuration already exists for ${ELEKTRON_ENV}"
+fi
+
+source "./etc/${ELEKTRON_ENV}.env" || (echo "Error: config for ${ELEKTRON_ENV} not found" && exit 1)
+
+ENV_NAME="${service_name}-${ELEKTRON_ENV}"
+
 if [ 'deploy-release' == "${CMD}" ];
 then
+    echo "Running deploy for ${ENV_NAME}"
+
     current_branch=$(git branch | grep \* | cut -d ' ' -f2)
     if [ "${current_branch}" != 'master' ];
     then
@@ -28,9 +39,9 @@ then
         exit 1
     fi
     
-    echo "Running unit tests."
+    echo "Running unit tests in development env."
     pushd project
-    python manage.py test
+    ELEKTRON_ENV=development python manage.py test
     result=$?
     popd
     if [ "${result}" != "0" ];
@@ -39,8 +50,10 @@ then
         exit ${result}
     fi
 
-    echo "Running integration test."
-    ./integration_test.py
+    echo "Running integration test in dev environment."
+    docker build --file Dockerfile-Proxy --tag roja/elektron_proxy:latest .
+    docker build --file Dockerfile-App --build-arg="ELEKTRON_ENV=development" --tag roja/elektron:latest .
+    ELEKTRON_ENV=development ./integration_test.py
     result=$?
     if [ "${result}" != "0" ];
     then
@@ -80,8 +93,27 @@ then
         # echo [$key] is pressed # uncomment to trace
         echo "Release tagging successful, deploying application version ${version}"
         git checkout ${version}
-        docker build --build-arg="ELEKTRON_ENV=${ELEKTRON_ENV}" --tag roja/elektron:${version} .
-        eb deploy --label "${version}" "${ENV_NAME}"
+
+        $(aws ecr get-login --no-include-email --region us-east-1)
+
+        ecr_repo='169122179348.dkr.ecr.us-east-1.amazonaws.com'
+
+        echo "Building a fresh image of Dockerfile-Proxy for ${ELEKTRON_ENV} at version ${version}"
+        docker build --file Dockerfile-Proxy --tag roja/elektron_proxy:${version} .
+        docker tag roja/elektron_proxy:${version} ${ecr_repo}/roja/elektron_proxy:${version}
+        docker push ${ecr_repo}/roja/elektron_proxy:${version}
+        tmp=$(mktemp)
+        cat Dockerrun.aws.json | sed "s/roja\/elektron_proxy:latest/roja\/elektron_proxy:${version}/" > "$tmp" && mv "$tmp" Dockerrun.aws.json
+
+        echo "Building a fresh image of Dockerfile-App for ${ELEKTRON_ENV} at version ${version}"
+        docker build --file Dockerfile-App --build-arg="ELEKTRON_ENV=${ELEKTRON_ENV}" --tag roja/elektron:${version} .
+        docker tag roja/elektron:${version} ${ecr_repo}/roja/elektron:${version}
+        docker push ${ecr_repo}/roja/elektron:${version}
+        tmp=$(mktemp)
+        cat Dockerrun.aws.json | sed "s/roja\/elektron:latest/roja\/elektron:${version}/" > "$tmp" && mv "$tmp" Dockerrun.aws.json
+
+        eb deploy "${ENV_NAME}"
+
         git checkout master
         exit $?
     else
