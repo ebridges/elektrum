1e8606e880f896d7f0ea082fdfbc0c4bef18c87f
Edward Bridges <github@eqbridges.com>
2019-01-10 05:31:55 -0500

    add additional test for asserting a login when address has not been validated

--- project/users/tests/test_authn_integration.py
+++ project/users/tests/test_authn_integration.py
@@ -32,17 +32,26 @@ class AuthnIntegrationTests(StaticLiveServerTestCase):
         d = json.load(f)
         self.data=d[0]
 
+
+  def test_login_with_verification(self):
     user = CustomUser.objects.get(username=self.data['fields']['username'])
     email = EmailAddress.objects.add_email(request=None, user=user, email=self.data['fields']['email'])
     email.verified = True
     email.save()
 
+    self.util_login_user(self.password)
+    self.assertInHTML('<a href="/account/logout/">Log out</a>', self.driver.page_source, count=1)
 
-  def test_login(self):
+
+  def test_login_without_verification(self):
+    self.util_login_user(self.password)
+    self.assertInHTML('<h1>Verify Your E-mail Address</h1>', self.driver.page_source, count=1)
+
+
+  def util_login_user(self, password):
     self.driver.get('%s%s' % (self.live_server_url, '/account/login/'))
     username_input = self.driver.find_element_by_name('login')
     username_input.send_keys(self.data['fields']['email'])
     password_input = self.driver.find_element_by_name('password')
-    password_input.send_keys(self.password)
+    password_input.send_keys(password)
     self.driver.find_element_by_xpath('//button').click()
-    self.assertInHTML('<a href="/account/logout/">Log out</a>', self.driver.page_source, count=1)
