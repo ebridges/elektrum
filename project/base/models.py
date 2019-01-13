b42e1105a324f0518b4673d1e1c346a60faeece9
Edward Bridges <github@eqbridges.com>
2019-01-13 07:46:38 -0500

    set up a base user class that overrides the `id` field to be a UUID4 by default

--- project/base/models.py
+++ project/base/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseUser(AbstractUser):
+    id = models.UUIDField(
+      _('id'),
+      primary_key=True,
+      default=uuid4,
+      editable=False,
+      max_length=64
+    )
