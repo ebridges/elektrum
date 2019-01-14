290e730b0ae8040269e5fbd2767c3eeddee10fce
Edward Bridges <github@eqbridges.com>
2019-01-14 18:32:56 -0500

    convert CustomUser to rely on the base model

--- project/users/models.py
+++ project/users/models.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.models import AbstractUser, UnicodeUsernameValidator
+import hashlib
+
+from django.contrib.auth.models import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.models import SocialAccount
-import hashlib
 
-class CustomUser(AbstractUser):
+from base.models import BaseUser
+
+class CustomUser(BaseUser):
     username_validator = UnicodeUsernameValidator()
 
     email = models.EmailField(blank=False, null=False, unique=True, max_length=512, verbose_name='email address')
