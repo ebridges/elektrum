a1096753b0cbbb5baf9897f920c0fd4fbe21c7a6
Edward Bridges <github@eqbridges.com>
2019-01-18 08:18:17 -0500

    wire up views

--- project/collection/views.py
+++ project/collection/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from collection.models import Collection
+
+class CollectionList(ListView):
+    model = Collection
+
+class CollectionView(DetailView):
+    model = Collection
+
+class CollectionCreate(CreateView):
+    model = Collection
+    fields = ['path']
+    success_url = reverse_lazy('collection_list')
+
+class CollectionUpdate(UpdateView):
+    model = Collection
+    fields = ['path']
+    success_url = reverse_lazy('collection_list')
+
+class CollectionDelete(DeleteView):
+    model = Collection
+    success_url = reverse_lazy('collection_list')
