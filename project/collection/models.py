a108c9026acf4f0fb32d38e9e30c7a96ca09f06c
Edward Bridges <github@eqbridges.com>
2019-01-18 05:40:38 -0500

    add collection model

--- project/collection/models.py
+++ project/collection/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.core import validators
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from base import BaseModel
+from users import CustomUser
+
+class Collection(BaseModel):
+    collection_path_validator = CollectionPathValidator()
+
+    path = models.CharField(
+      _('collection path'),
+      verbose_name=_('Collection Path'),
+      help_text=_('Required. Path to media collection from the root of your archive.'),
+      error_messages={
+          'unique': _('A collection with that path already exists.'),
+      },
+      validators=[collection_path_validator],
+      unique=True, 
+      max_length=1024,
+    )
+
+    user_id = models.ForeignKey(
+      CustomUser, 
+      null=False, 
+      help_text=_('User that owns this collection'),
+    )
+
+    def __str__(self):
+        return self.path
+
+    def name(self):
+        return self.path[1:]
+
+
+  class Meta:
+      unique_together = (
+        ('path', 'user_id'),
+      )
+
+
+@deconstructible
+class CollectionPathValidator(validators.RegexValidator):
+    regex = r'^\/[0-9]{4}$'
+    message = _(
+        'Enter a valid collection path. This value may only be a 4 digit year, '
+        'with a leading slash.'
+    )
+    flags = 0
