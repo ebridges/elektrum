7cea11d970361e8e9967ae648d4f99189b758d82
Edward Bridges <github@eqbridges.com>
2019-01-19 06:16:26 -0500

    add templates & tests, both of which are in intermediate state, to pause work on collections

---
+++ project/collection/tests/test_collection_crud.py
@@ -0,0 +1,23 @@
+import re
+import os
+import json
+import email
+
+from django.test import Client, TestCase
+
+from users.models import CustomUser
+
+class AuthnUserFlowTest(TestCase):
+  fixtures = ['users/tests/user-data.json']
+
+  def setUp(self):
+    self.email_log_dir = './sent_emails'
+    self.password='temporary'
+    with open('users/tests/user-data.json') as f:
+        d = json.load(f)
+        self.data=d
+
+  def test_signup_flow(self):
+    c = Client()
+    response = c.post('/account/signup/', {'username': 'newuser', 'email': 'newuser@example.com', 'first_name': 'first', 'last_name': 'last', 'password1': 'abcd@1234', 'password2': 'abcd@1234'})
+    self.util_assert_account_redirects(response)
