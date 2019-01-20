63b3abf8e87a7239346d7b2a25fa640635da971b
Edward Bridges <github@eqbridges.com>
2019-01-20 06:19:02 -0500

    build up a context for homepage that allows for displaying two forms at once (signup & login).

--- project/pages/views.py
+++ project/pages/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from allauth.account.views import SignupView
+from allauth.account.forms import LoginForm
 
-class HomePageView(TemplateView):
+class HomePageView(SignupView):
     template_name = 'home.html'
+    # here we add some context to the already existing context
+    def get_context_data(self, **kwargs):
+        # we get context data from original view
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['login_form'] = LoginForm() # add form to context
+        return context
+
