126dd9b416fe4caa41d10cf017beb04c71ef2519
Edward Bridges <github@eqbridges.com>
2019-01-21 06:12:43 -0500

    convert to function-based views

--- project/collection/urls.py
+++ project/collection/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CollectionList.as_view(), name='collection_list'),
-    path('view/<uuid:pk>', views.CollectionView.as_view(), name='collection_view'),
-    path('new', views.CollectionCreate.as_view(), name='collection_new'),
-    path('edit/<uuid:pk>', views.CollectionUpdate.as_view(), name='collection_edit'),
-    path('delete/<uuid:pk>', views.CollectionDelete.as_view(), name='collection_delete'),
+    path('', views.collection_list, name='collection_list'),
+    path('view/<uuid:pk>', views.collection_view, name='collection_view'),
+    path('new', views.collection_create, name='collection_new'),
+    path('edit/<uuid:pk>', views.collection_edit, name='collection_edit'),
+    path('delete/<uuid:pk>', views.collection_delete, name='collection_delete'),
 ]
--- project/collection/views.py
+++ project/collection/views.py
@@ -1,26 +1,45 @@
-from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import ModelForm
 
 from collection.models import Collection
 
-class CollectionList(ListView):
-    model = Collection
+class CollectionForm(ModelForm):
+    class Meta:
+        model = Collection
+        fields = ['path']
 
-class CollectionView(DetailView):
-    model = Collection
+def collection_list(request, template_name='collection/collection_list.html'):
+    collection = Collection.objects.all()
+    data = {}
+    data['object_list'] = collection
+    return render(request, template_name, data)
 
-class CollectionCreate(CreateView):
-    model = Collection
-    fields = ['path']
-    success_url = reverse_lazy('collection_list')
 
-class CollectionUpdate(UpdateView):
-    model = Collection
-    fields = ['path']
-    success_url = reverse_lazy('collection_list')
+def collection_view(request, pk, template_name='collection/collection_detail.html'):
+    collection= get_object_or_404(Collection, pk=pk)    
+    return render(request, template_name, {'object':collection})
 
-class CollectionDelete(DeleteView):
-    model = Collection
-    success_url = reverse_lazy('collection_list')
+
+def collection_create(request, template_name='collection/collection_form.html'):
+    form = CollectionForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('collection_list')
+    return render(request, template_name, {'form':form})
+
+
+def collection_edit(request, pk, template_name='collection/collection_form.html'):
+    collection= get_object_or_404(Collection, pk=pk)
+    form = CollectionForm(request.POST or None, instance=collection)
+    if form.is_valid():
+        form.save()
+        return redirect('collection_list')
+    return render(request, template_name, {'form':form})
+
+
+def collection_delete(request, pk, template_name='collection/collection_confirm_delete.html'):
+    collection= get_object_or_404(Collection, pk=pk)    
+    if request.method=='POST':
+        collection.delete()
+        return redirect('collection_list')
+    return render(request, template_name, {'object':collection})
