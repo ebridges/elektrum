fe17262d3b87454269557b8e6cc8f630ef67b8dc
Edward Bridges <github@eqbridges.com>
2019-01-22 06:01:32 -0500

    unit tests for edge cases

--- project/collection/tests/test_collection_crud.py
+++ project/collection/tests/test_collection_crud.py
@@ -27,9 +27,61 @@ class AuthnUserFlowTest(TestCase):
     self.util_assert_account_redirects(response)
 
 
-# create a collection with invalid path, expect failure
-# create a collection with missing path, expect failure
-# create a collection with duplicate path, expect failure
+  def test_create_collection_invalid_paths(self):
+    '''
+    Create collections with invalid paths, expect failures
+    '''
+    invalid_path_msg='<li>Enter a valid collection path. This value may only be a 4 digit year, with a leading slash.</li>'
+    c = Client()
+    login_result = c.login(email=self.data[0]['fields']['email'], password=self.password)
+    self.assertTrue(login_result)
+
+    response = c.post('/collections/new', {'path': '/asdfasdf'})
+    self.assertIsNotNone(response)
+    self.assertContains(response, invalid_path_msg)
+
+    response = c.post('/collections/new', {'path': 'asdf'})
+    self.assertIsNotNone(response)
+    self.assertContains(response, invalid_path_msg)
+
+    response = c.post('/collections/new', {'path': '3030'})
+    self.assertIsNotNone(response)
+    self.assertContains(response, invalid_path_msg)
+
+  def test_create_collection_missing_paths(self):
+    '''
+    Create a collection with missing path, expect failure
+    '''
+    c = Client()
+    login_result = c.login(email=self.data[0]['fields']['email'], password=self.password)
+    self.assertTrue(login_result)
+
+    missing_path_msg='<li>This field is required.</li>'
+    response = c.post('/collections/new', {'path': ''})
+    self.assertIsNotNone(response)
+    self.assertContains(response, missing_path_msg)
+
+
+  def test_create_collection_duplicate_paths(self):
+    '''
+    Create a collection with duplicate path, expect failure
+    '''
+    c = Client()
+    login_result = c.login(email=self.data[0]['fields']['email'], password=self.password)
+    self.assertTrue(login_result)
+
+    duplicate_path_msg='<li>A collection with that path already exists.</li>'
+
+    response = c.post('/collections/new', {'path': '/3030'})
+    self.assertIsNotNone(response)
+    self.util_assert_account_redirects(response)
+
+    response = c.post('/collections/new', {'path': '/3030'})
+    self.assertIsNotNone(response)
+    self.assertContains(response, duplicate_path_msg)
+
+
+
 # delete a user and confirm collection is deleted
 # attempt to list/edit/create/delete a collection when not authenticated (user#is_authenticated is False), expect failure
 # attempt to list/edit/create/delete another user's collection, expect failure
