7b56a6bb1d31870af4a4f14c6de09689822e68ae
Edward Bridges <github@eqbridges.com>
2019-01-21 20:42:30 -0500

    add test for creating a collection

--- project/collection/tests/test_collection_crud.py
+++ project/collection/tests/test_collection_crud.py
@@ -10,13 +10,21 @@ class AuthnUserFlowTest(TestCase):
   fixtures = ['users/tests/user-data.json']
 
   def setUp(self):
-    self.email_log_dir = './sent_emails'
     self.password='temporary'
     with open('users/tests/user-data.json') as f:
         d = json.load(f)
         self.data=d
 
-  def test_signup_flow(self):
+  # create a collection
+  def test_create_collection(self):
     c = Client()
-    response = c.post('/account/signup/', {'username': 'newuser', 'email': 'newuser@example.com', 'first_name': 'first', 'last_name': 'last', 'password1': 'abcd@1234', 'password2': 'abcd@1234'})
-    self.util_assert_account_redirects(response)
+    c.login(email=self.data[0]['fields']['email'], password=self.data[0]['fields']['password'])
+    response = c.post('', {'path': '/3030'})
+    self.assertIsNotNone(response)
+
+# create a collection with invalid path, expect failure
+# create a collection with missing path, expect failure
+# create a collection with duplicate path, expect failure
+# delete a user and confirm collection is deleted
+# attempt to list/edit/create/delete a collection when not authenticated (user#is_authenticated is False), expect failure
+# attempt to list/edit/create/delete another user's collection, expect failure
