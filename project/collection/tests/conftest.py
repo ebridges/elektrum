3a2bf94fd9024d1091d50a61717c94d7f4b0544f
Edward Bridges <github@eqbridges.com>
2019-02-08 18:36:40 -0500

    switch to use fixtures from users module

--- project/collection/tests/conftest.py
+++ project/collection/tests/conftest.py
@@ -1,10 +1,11 @@
 from pytest_factoryboy import register
 
 from collection.tests.factories import CollectionFactory
+from users.tests.factories import UserFactory
 
-
+register(UserFactory)
 register(CollectionFactory)
 
 pytest_plugins = [
-     "base.tests.fixtures"
+     "users.tests.fixtures"
 ]
