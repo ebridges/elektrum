e1750b1c771cae2d58fdeabe2f27caa9c1707ac3
Edward Bridges <github@eqbridges.com>
2019-02-23 06:45:07 -0500

    add tests to bring coverage to 100%

--- processor/src/test/java/cc/roja/photo/util/DateUtilsTest.java
+++ processor/src/test/java/cc/roja/photo/util/DateUtilsTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAccessor;
+import java.util.Optional;
+import java.util.TimeZone;
 
 import static cc.roja.photo.util.DateUtils.getDateValueFromMetadata;
 import static cc.roja.photo.util.DateUtils.parseDateWithDefaults;
+import static cc.roja.photo.util.DateUtils.stripTimeZone;
 import static java.time.temporal.ChronoField.DAY_OF_MONTH;
 import static java.time.temporal.ChronoField.HOUR_OF_DAY;
 import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
@@ -50,6 +55,29 @@ class DateUtilsTest {
     assertTemporalAccessor(expected, actual);
   }
 
+  @ParameterizedTest
+  @CsvSource({
+      "2020:02:26 12:34:56Z, yyyy:MM:dd HH:mm:ssVV, GMT",
+      "2020-02-26T12:34:56Z, yyyy-MM-dd'T'HH:mm:ssVV, GMT",
+      "2020:02:26 12:34:56+05:00, yyyy:MM:dd HH:mm:ssVV, GMT+05:00",
+      "2020-02-26T12:34:56+05:00, yyyy-MM-dd'T'HH:mm:ssVV, GMT+05:00",
+      "2020:02:26 12:34:56-05:00, yyyy:MM:dd HH:mm:ssVV, GMT-05:00",
+      "2020-02-26T12:34:56-05:00, yyyy-MM-dd'T'HH:mm:ssVV, GMT-05:00",
+  })
+  void getDateValueFromMetadata_WithTZSuccess(String testCase, String pattern, String timeZone) {
+    int tagType = 999;
+    TimeZone tz = TimeZone.getTimeZone(timeZone);
+    Directory mockDirectory = mock(Directory.class);
+    when(mockDirectory.getObject(tagType)).thenReturn(testCase);
+    TemporalAccessor actual = getDateValueFromMetadata(mockDirectory, tagType);
+    TemporalAccessor expected = parseDateWithDefaults(testCase, pattern, Optional.of(tz));
+    assertTemporalAccessor(expected, actual);
+    ZonedDateTime expectedWithZone = ZonedDateTime.from(expected);
+    assert actual != null;
+    ZonedDateTime actualWithZone = ZonedDateTime.from(actual);
+    assertEquals(expectedWithZone.getOffset(), actualWithZone.getOffset());
+  }
+
   @Test
   void getDateValueFromMetadata_TagTypeNotFound() {
     int tagType = 0;
@@ -80,6 +108,15 @@ class DateUtilsTest {
     String actual = dtf.format(underTest);
     assertEquals(testCase, actual);
   }
+
+  @Test
+  void testStripTimeZone() {
+    LocalDateTime expected = LocalDateTime.of(2020, 2, 26, 0, 0, 0);
+    ZonedDateTime zonedDateTime = ZonedDateTime.of(expected, TimeZone.getTimeZone("America/New_York").toZoneId());
+    LocalDateTime actual = stripTimeZone(zonedDateTime);
+    assertTemporalAccessor(expected, actual);
+  }
+
   private void assertTemporalAccessor(TemporalAccessor expected, TemporalAccessor actual) {
     assertNotNull(expected);
     assertNotNull(actual);
