0f1b4ddfd8234ce83793a2fdf079547851d1323f
Edward Bridges <github@eqbridges.com>
2019-02-24 17:56:07 -0500

    S3ImageLoader test

--- processor/src/main/java/cc/roja/photo/io/S3ImageLoader.java
+++ processor/src/main/java/cc/roja/photo/io/S3ImageLoader.java
@@ -13,26 +13,43 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
+import static cc.roja.photo.io.ImageLoaderFactory.ENV_BUCKET_NAME;
 import static java.lang.System.getenv;
 
 class S3ImageLoader implements ImageLoader {
   private static final Logger LOG = Logger.getLogger(S3ImageLoader.class);
+
+  private AmazonS3 s3Client;
+
+  S3ImageLoader() {
+    this.s3Client = AmazonS3ClientBuilder.defaultClient();
+  }
+
+  // For testing
+  S3ImageLoader(AmazonS3 s3Client) {
+    this.s3Client = s3Client;
+  }
+
   @Override
   public File load(String imageKey) throws IOException {
+    if(imageKey == null || imageKey.isEmpty()) {
+      throw new IOException("imageKey cannot be empty or null.");
+    }
+
     // download from S3 & store in temp location
-    String bucket = getenv(ImageLoaderFactory.ENV_BUCKET_NAME);
+    String bucket = getenv(ENV_BUCKET_NAME);
+    if(bucket == null || bucket.isEmpty()) {
+      throw new IOException("bucket name not configured under env var ["+ENV_BUCKET_NAME+"]");
+    }
     String extension = FilenameUtils.getExtension(imageKey);
     Path tempfile = Files.createTempFile(bucket, extension);
 
-    String prefixedKey = "photos/pictures" + imageKey;
-
-    AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
-    S3Object object = s3Client.getObject(bucket, prefixedKey);
+    S3Object object = s3Client.getObject(bucket, imageKey);
     try (InputStream in = object.getObjectContent()) {
       Files.copy(in, tempfile, StandardCopyOption.REPLACE_EXISTING);
     }
 
-    LOG.info("downloaded key: "+prefixedKey + " to " + tempfile);
+    LOG.info("downloaded key: "+imageKey + " to " + tempfile);
 
     return tempfile.toFile();
   }
