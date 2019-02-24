458aa5834ecf49bfc17d005a799b1e872f7b06ce
Edward Bridges <github@eqbridges.com>
2019-02-24 07:34:54 -0500

    further reduce visibility of metadata handling

--- processor/src/main/java/cc/roja/photo/metadata/MetadataUtils.java
+++ processor/src/main/java/cc/roja/photo/metadata/MetadataUtils.java
@@ -33,7 +33,7 @@ class MetadataUtils {
       of(ExifIFD0Directory.class, TAG_DATETIME),
   };
 
-  public static Rational resolveRational(Metadata metadata, TagPair... tags) {
+  static Rational resolveRational(Metadata metadata, TagPair... tags) {
     Rational value = null;
     for(TagPair tagPair : tags) {
       LOG.debug("tagPair: "+ tagPair);
@@ -46,7 +46,7 @@ class MetadataUtils {
     return value;
   }
 
-  public static Rational[] resolveRationalArray(Metadata metadata, TagPair... tags) {
+  static Rational[] resolveRationalArray(Metadata metadata, TagPair... tags) {
     Rational[] value = null;
     for(TagPair tagPair : tags) {
       LOG.debug("tagPair: "+ tagPair);
@@ -59,7 +59,7 @@ class MetadataUtils {
     return value;
   }
 
-  public static TemporalAccessor resolveDate(Metadata metadata, TagPair... tags) {
+  static TemporalAccessor resolveDate(Metadata metadata, TagPair... tags) {
     TemporalAccessor value;
     for(TagPair tagPair : tags) {
       LOG.debug("tagPair: "+ tagPair);
@@ -75,7 +75,7 @@ class MetadataUtils {
     return null;
   }
 
-  public static String resolveString(Metadata metadata, TagPair... tags) {
+  static String resolveString(Metadata metadata, TagPair... tags) {
     String value = null;
     for(TagPair tagPair : tags) {
       LOG.debug("tagPair: "+ tagPair);
@@ -92,7 +92,7 @@ class MetadataUtils {
     return value;
   }
 
-  public static Integer resolveInteger(Metadata metadata, TagPair... tags) {
+  static Integer resolveInteger(Metadata metadata, TagPair... tags) {
     Integer value = null;
     for(TagPair tagPair : tags) {
       LOG.debug("tagPair: "+ tagPair);
@@ -105,7 +105,7 @@ class MetadataUtils {
     return value;
   }
 
-  public static String resolveDescription(Metadata metadata, TagPair... tags) {
+  static String resolveDescription(Metadata metadata, TagPair... tags) {
     String value = null;
     for(TagPair tagPair : tags) {
       LOG.debug("tagPair: "+ tagPair);
@@ -127,7 +127,7 @@ class MetadataUtils {
     return null;
   }
 
-  public static Directory getDirectory(Metadata meta, Class<? extends Directory> dirClass) {
+  static Directory getDirectory(Metadata meta, Class<? extends Directory> dirClass) {
     if(meta.containsDirectoryOfType(dirClass)) {
       return meta.getFirstDirectoryOfType(dirClass);
     } else {
@@ -152,7 +152,7 @@ class MetadataUtils {
    * @return TemporalAccessor
    */
   @SuppressWarnings({"SameParameterValue", "JavadocReference"})
-  public static TemporalAccessor getDateValueFromMetadata(Directory dir, int tagType) {
+  static TemporalAccessor getDateValueFromMetadata(Directory dir, int tagType) {
     Object o = dir.getObject(tagType);
 
     if(o == null) {
