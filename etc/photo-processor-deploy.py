2bfbe39f5f93b1978c2e9d60c40b3946325ad2cc
Edward Bridges <github@eqbridges.com>
2019-03-01 18:42:29 -0500

    add packaging & deployment for lambda function

---
+++ etc/photo-processor-deploy.py
@@ -0,0 +1,71 @@
+from logging import Logger, basicConfig, INFO, DEBUG, CRITICAL, debug, info, getLogger
+from json import dumps
+from os import getenv
+from sys import argv
+from dotenv import load_dotenv
+import boto3
+
+# Presumed to be run from the root of the project
+
+def deploy(archive):
+  debug('Deploying lambda function for [photo-processor]')
+  client = boto3.client('lambda')
+  response = client.list_functions()
+  for f in response['Functions']:
+    if f['FunctionName'] == 'photo-processor':
+      debug('Found existing function [%s].' % f['FunctionName'])
+      client.delete_function(FunctionName='photo-processor')
+      debug('Existing function [%s] deleted.' % f['FunctionName'])
+      break
+  create_function(client, archive)
+  info('Function created [photo-processor]')
+
+
+def create_function(client, archive):
+  env = getenv('ELEKTRON_ENV', 'development')
+  env_file = "etc/%s.env" % env
+  debug('Loading environment from [%s]' % env_file)
+  load_dotenv(dotenv_path=env_file, verbose=True)
+
+  db_jdbc_url = 'jdbc:postgresql://%s:%s/%s' % (getenv('db_hostname'), getenv("db_port_num"), getenv('db_name'))
+
+  debug('Uploading archive [%s]' % archive)
+  with open(archive, "rb") as binaryfile :
+    zipfile = bytearray(binaryfile.read())
+    client.create_function(
+      FunctionName='photo-processor', 
+      Runtime='java8', 
+      Role=getenv('photo_processor_role_arn'),
+      Handler='cc.roja.photo.ProcessoreRequestHandler',
+      Code={ 'ZipFile': zipfile },
+      Description='Processes photos.',
+      Timeout=int(getenv('connection_timeout')),
+      MemorySize=int(getenv('memory_size')),
+      Publish=True,
+      VpcConfig={
+          'SubnetIds': getenv('vpc_public_subnet_ids').split(','),
+          'SecurityGroupIds': getenv('vpc_security_group_ids').split(',')
+      },
+      Environment={
+        'Variables': {
+            'DB_JDBC_URL': db_jdbc_url,
+            'DB_USERNAME': getenv('db_username'),
+            'DB_PASSWORD': getenv('db_password'),
+            'BUCKET_NAME': getenv('bucket_name')
+        }
+      }
+    )
+
+def configure_logging(threshold=INFO):
+  basicConfig(
+      format='[%(asctime)s][%(levelname)s] [%(name)s] %(message)s',
+      datefmt='%Y/%m/%d %H:%M:%S',
+      level=threshold)
+  getLogger('botocore.credentials').setLevel(CRITICAL)
+
+if __name__ == "__main__":
+  if len(argv) > 2:
+    configure_logging(DEBUG)
+  else:
+    configure_logging()
+  deploy(argv[1])
