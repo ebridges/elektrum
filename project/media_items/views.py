d2885d8b8e14ff12cb1e6bfda4705a3662a914f6
Edward Bridges <github@eqbridges.com>
2019-05-06 12:39:02 -0400

    dont record state about the upload before processing has occurred

--- project/media_items/views.py
+++ project/media_items/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequ
 from django.views import View
 from django.utils.dateparse import parse_datetime
 
-from media_items.upload_signing import create_signed_upload_url, record_upload_request, supported_upload_types, split_upload_path
+from media_items.upload_signing import create_signed_upload_url, supported_upload_types
 
 
 class SignRequest(View):
@@ -24,13 +24,9 @@ class SignRequest(View):
         if mime_type not in supported_upload_types:
             return HttpResponseBadRequest('the provided mime type [%s] is not supported.' % mime_type)
 
-
-        item_id = record_upload_request(user, signed_url, create_date, mime_type)
         signed_url = create_signed_upload_url(user, mime_type)
 
         response = HttpResponse(status=201)
         response['Location'] = signed_url.geturl()
-        response['X-Elektron-Media-Id'] = item_id
-        response['X-Elektron-Filename'] = split_upload_path(signed_url)[2]
 
         return response
