ea6b04d0cf2574978d8accc0e606364e838c6dcb
Edward Bridges <github@eqbridges.com>
2019-05-11 20:48:17 -0400

    add view for listing collections for an owner, along with a positive case test

--- project/media_items/tests/test_media_item_views.py
+++ project/media_items/tests/test_media_item_views.py
@@ -9,3 +9,15 @@ def test_media_item_create(media_item_factory):
     assert mi.create_date.year == mi.create_day.year
     assert mi.create_date.month == mi.create_day.month
     assert mi.create_date.day == mi.create_day.day
+
+
+@pytest.mark.django_db
+def test_media_item_collections_view(media_item_factory, authenticated_client):
+    c, u = authenticated_client
+    mi = media_item_factory(owner=u)
+    request_url = '/media/%s/' % u.id
+    response = c.get(request_url)
+    content = str(response.content)
+    assert response.status_code == 200
+    assert 'Collections available for %s' % u.username in content
+    assert '<td>%s</td>' % mi.create_day.year in content
--- project/media_items/views/media_views.py
+++ project/media_items/views/media_views.py
@@ -1,3 +1,10 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+from base.views.errors import *
+from media_items.models import MediaItem
+from date_dimension.models import DateDimension
+
+
 def media_item_view():
     pass
 
@@ -10,6 +17,14 @@ def albums_view():
     pass
 
 
-def collections_view():
-    pass
+@exceptions_to_http_status
+def collections_view(request, owner_id, template_name='media_items/collections_view.html'):
+    assert_owner_id(owner_id, request.user.id)
+    dates = DateDimension.objects.filter(mediaitem__owner_id__exact=owner_id)
+    data = {'object_list': [d.year for d in dates]}
+    return render(request, template_name, data)
 
+
+def assert_owner_id(owner_id, user_id):
+    if owner_id != user_id:
+        raise BadRequestException('User is not authenticated properly')
