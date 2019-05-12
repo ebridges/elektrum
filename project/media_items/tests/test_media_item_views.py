cc3ee4ed6337c5e23bd7cb0e18e846be5f4b97e7
Edward Bridges <github@eqbridges.com>
2019-05-12 05:09:50 -0400

    remove unused test

--- project/media_items/tests/test_media_item_views.py
+++ project/media_items/tests/test_media_item_views.py
@@ -3,16 +3,6 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_media_item_create(media_item_factory):
-    mi = media_item_factory()
-    assert mi is not None
-    assert mi.create_day is not None
-    assert mi.create_date.year == mi.create_day.year
-    assert mi.create_date.month == mi.create_day.month
-    assert mi.create_date.day == mi.create_day.day
-
-
-@pytest.mark.django_db
 def test_media_item_collections_view(media_item_factory, authenticated_client):
     c, u = authenticated_client
     mi = media_item_factory(owner=u)
