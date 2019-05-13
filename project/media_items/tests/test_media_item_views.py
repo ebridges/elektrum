895e11d9b4487d89a573953f4d07f644fceb860d
Edward Bridges <github@eqbridges.com>
2019-05-13 06:16:44 -0400

    add albums_view view along with test

--- project/media_items/tests/test_media_item_views.py
+++ project/media_items/tests/test_media_item_views.py
@@ -22,3 +22,23 @@ def test_media_item_collections_view_bad_request(media_item_factory, authenticat
     response = c.get(request_url)
     assert response.status_code == 400
 
+
+@pytest.mark.django_db
+def test_media_item_albums_view(media_item_factory, authenticated_client):
+    c, u = authenticated_client
+    mi = media_item_factory(owner=u)
+    request_url = '/media/%s/%s/' % (u.id, mi.create_day.year)
+    response = c.get(request_url)
+    content = str(response.content)
+    assert response.status_code == 200
+    assert 'Albums available for %s in year %s' % (u.username, mi.create_day.year) in content
+    assert '<td>%s</td>' % mi.create_day.yyyymmdd in content
+
+
+@pytest.mark.django_db
+def test_media_item_albums_view_bad_request(media_item_factory, authenticated_client):
+    c, u = authenticated_client
+    media_item_factory(owner=u)
+    request_url = '/media/%s/' % uuid4()
+    response = c.get(request_url)
+    assert response.status_code == 400
--- project/media_items/views/media_views.py
+++ project/media_items/views/media_views.py
@@ -13,8 +13,12 @@ def media_list_view():
     pass
 
 
-def albums_view():
-    pass
+@exceptions_to_http_status
+def albums_view(request, owner_id, year, template_name='media_items/albums_view.html'):
+    assert_owner_id(owner_id, request.user.id)
+    dates = DateDimension.objects.filter(mediaitem__owner_id__exact=owner_id, year__exact=year)
+    data = {'object_list': [d.yyyymmdd for d in dates], 'collection_year': year}
+    return render(request, template_name, data)
 
 
 @exceptions_to_http_status
