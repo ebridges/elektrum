a10668c599e096de68f886550cbd7cea99816075
Edward Bridges <github@eqbridges.com>
2019-05-14 08:40:23 -0400

    add basic test for individual media item

--- project/media_items/tests/test_media_item_views.py
+++ project/media_items/tests/test_media_item_views.py
@@ -69,3 +69,25 @@ def test_media_item_list_view_bad_request(media_item_factory, authenticated_clie
     request_url = '/media/%s/%s/%s/' % (uuid4(), mi.create_day.year, mi.create_day.iso_date)
     response = c.get(request_url)
     assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_media_item_view(authenticated_client, media_item_factory):
+    c, u = authenticated_client
+    mi = media_item_factory(owner=u)
+    request_url = '/media/%s/%s/' % (u.id, mi.id)
+    response = c.get(request_url)
+    content = str(response.content)
+
+    assert response.status_code == 200
+    assert 'Media Item for %s date: %s' % (u.username, mi.create_day.iso_date) in content
+    assert '<td>%s</td>' % mi.id in content
+
+
+@pytest.mark.django_db
+def test_media_item_view_bad_request(media_item_factory, authenticated_client):
+    c, u = authenticated_client
+    mi = media_item_factory(owner=u)
+    request_url = '/media/%s/%s/' % (uuid4(), mi.id)
+    response = c.get(request_url)
+    assert response.status_code == 400
