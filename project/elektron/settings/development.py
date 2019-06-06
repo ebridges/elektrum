78a17ab82d007fe596c34a08e2247b320e9fc2c1
Edward Bridges <github@eqbridges.com>
2019-06-06 10:12:51 -0400

    add API gateway to ALLOWED_HOSTS

--- project/elektron/settings/development.py
+++ project/elektron/settings/development.py
@@ -1,6 +1,6 @@
 from .base import *
 DEBUG = True
-ALLOWED_HOSTS = ['testserver', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['testserver', '127.0.0.1', 'localhost', '.execute-api.us-east-1.amazonaws.com']
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = './sent_emails'
 TEST_RUNNER = 'elektron.test_runner.PytestTestRunner'
