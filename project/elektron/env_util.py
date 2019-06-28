993589d211e656ffb630f282b74666f93f03e419
Edward Bridges <github@eqbridges.com>
2019-06-28 10:52:29 -0400

    clean up debug logs

--- project/elektron/env_util.py
+++ project/elektron/env_util.py
@@ -7,21 +7,21 @@ def locate_env_file(start_dir=os.getcwd()):
 
   # check in current directory for `.env`
   location = os.path.join(start_dir, '.env')
-  #print('Looking for env file in %s' % location)
+  #print('[DEBUG] Looking for env file in %s' % location)
   if os.path.isfile(location):
     print('[INFO] Located env file in %s' % location)
     return location
 
   # check in parent, parent directory for `.env`
   location = os.path.join(os.path.dirname(start_dir), '.env')
-  #print('Looking for env file in %s' % location)
+  #print('[DEBUG] Looking for env file in %s' % location)
   if os.path.isfile(location):
     print('[INFO] Located env file in %s' % location)
     return location
   
   # check in parent, parent etc/env directory for `$ELEKTRON_ENV.env`
   location = os.path.join(os.path.dirname(start_dir), 'etc/env/%s.env' % os.getenv('ELEKTRON_ENV', DEFAULT_ENV))
-  #print('Looking for env file in %s' % location)
+  #print('[DEBUG] Looking for env file in %s' % location)
   if os.path.isfile(location):
     print('[INFO] Located env file in %s' % location)
     return location
