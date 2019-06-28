40dc6f9c228b8b75d8b67e36f5598f9429f75059
Edward Bridges <github@eqbridges.com>
2019-06-28 06:31:54 -0400

    fix base_dir

--- project/elektron/settings.py
+++ project/elektron/settings.py
@@ -16,8 +16,7 @@ from elektron.env_util import locate_env_file, resolve_version
 import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.abspath('%s/../..' % os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env_file = locate_env_file(BASE_DIR)
 dotenv.read_dotenv(env_file)
