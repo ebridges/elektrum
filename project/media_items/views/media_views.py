cf6e72ffe536a3077278fb7e131f0f58c492b245
Edward Bridges <github@eqbridges.com>
2019-07-29 21:36:43 -0400

    refactor out to a util method

--- project/media_items/views/media_views.py
+++ project/media_items/views/media_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+from base.views.utils import assert_owner_id
 from base.views.errors import exceptions_to_http_status, BadRequestException
 from media_items.models import MediaItem
 from date_dimension.models import DateDimension
@@ -36,8 +37,3 @@ def collections_view(request, owner_id, template_name='media_items/collections_v
     dates = DateDimension.objects.filter(mediaitem__owner_id__exact=owner_id)
     data = {'object_list': [d.year for d in dates]}
     return render(request, template_name, data)
-
-
-def assert_owner_id(owner_id, user_id):
-    if owner_id != user_id:
-        raise BadRequestException('User is not authenticated properly')
