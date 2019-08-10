4c21b7aceb5fcc1a172a612e3c432f371f49587a
Edward Bridges <github@eqbridges.com>
2019-08-10 09:57:30 -0400

    remove the now unnecessary app-home view

--- project/pages/urls.py
+++ project/pages/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('app-home', views.app_home_page_view, name='app-home'),
+    # path('app-home', views.app_home_page_view, name='app-home'),
 ]
--- project/pages/views.py
+++ project/pages/views.py
@@ -1,13 +1,5 @@
-from django.views.generic import TemplateView
-from django.shortcuts import render
-
 from allauth.account.views import SignupView
 from allauth.account.forms import LoginForm
-
-from base.views.errors import exceptions_to_http_status
-from base.views.utils import media_url
-from date_dimension.models import DateDimension
-from media_items.models import MediaItem
 
 
 class HomePageView(SignupView):
@@ -19,16 +11,3 @@ class HomePageView(SignupView):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['login_form'] = LoginForm()  # add form to context
         return context
-
-
-@exceptions_to_http_status
-def app_home_page_view(request, template_name='app-home.html'):
-    media_items = MediaItem.objects.raw('''select distinct on (d.year) m.* 
-                                    from media_item m, date_dim d 
-                                    where m.create_day_id = d.yyyymmdd 
-                                    order by d.year, random()''')
-    data = []
-    for mi in media_items:
-        data.append({'year': str(mi.create_day_id)[:4], 'url': media_url(mi.file_path)})
-
-    return render(request, template_name, {'objects': data})
