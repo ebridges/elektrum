92ce36f3fd26914ae90e9192dcb13c6d795b4d70
Edward Bridges <github@eqbridges.com>
2019-08-12 19:32:59 -0400

    whitespace

--- project/base/views/utils.py
+++ project/base/views/utils.py
@@ -3,9 +3,9 @@ from base.views.errors import BadRequestException
 
 
 def assert_owner_id(owner_id, user_id):
-    if owner_id != user_id:
-        raise BadRequestException('User is not authenticated properly')
+  if owner_id != user_id:
+    raise BadRequestException('User is not authenticated properly')
 
 
 def media_url(path, scheme='https'):
-        return '%s://%s%s' % (scheme, environ.get('media_storage_cname'), path)
+  return '%s://%s%s' % (scheme, environ.get('media_storage_cname'), path)
