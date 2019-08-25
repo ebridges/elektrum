3c28300682dff35e2faa8275077bec551a559b9e
Edward Bridges <github@eqbridges.com>
2019-08-24 22:40:13 -0400

    update tool to work from new directory

--- tools/encrypt-var.sh
+++ tools/encrypt-var.sh
@@ -8,4 +8,4 @@ then
     exit 1
 fi
 
-pbpaste | ansible-vault encrypt_string --vault-password-file=network/environments/${env}-vault-password.txt --stdin-name ${var_name} | pbcopy
+pbpaste | ansible-vault encrypt_string --vault-password-file=../network/environments/${env}-vault-password.txt --stdin-name ${var_name} | pbcopy
