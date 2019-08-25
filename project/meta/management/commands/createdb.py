e8202daf10fd4de5379db4d14cf6bf85c1b01f73
Edward Bridges <github@eqbridges.com>
2019-08-24 22:37:36 -0400

    add management command to create an admin account for a django project

--- project/meta/management/commands/createdb.py
+++ project/meta/management/commands/createdb.py
@@ -17,17 +17,11 @@ class Command(BaseCommand):
         password = settings.DATABASES['default']['PASSWORD']
         host = settings.DATABASES['default']['HOST']
 
-        connection = None
-        try:
-          connection = connect(dbname='postgres', user=user, host=host, password=password)
-          cursor = connection.cursor()
-          if (cursor.execute("select not exists(SELECT datname FROM pg_catalog.pg_database WHERE datname = '%s')" % dbname)):
-            connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-            cursor.execute("CREATE DATABASE '%s'" % dbname)
-            self.stdout.write(self.style.SUCCESS('Successfully created database [%s]' % dbname))
-          else:
-            self.stdout.write(self.style.SUCCESS('Database [%s] already exists.' % dbname))
-        finally:
-          if (connection and connection.closed == 0):
-            cursor.close()
-            connection.close()
+        with connect(dbname='postgres', user=user, host=host, password=password) as connection:
+          with connection.cursor() as cursor:
+            if (cursor.execute("select not exists(SELECT datname FROM pg_catalog.pg_database WHERE datname = '%s')" % dbname)):
+              connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+              cursor.execute("CREATE DATABASE '%s'" % dbname)
+              self.stdout.write(self.style.SUCCESS('Successfully created database [%s]' % dbname))
+            else:
+              self.stdout.write(self.style.SUCCESS('Database [%s] already exists.' % dbname))
--- project/status/urls.py
+++ project/status/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('ok/', views.Ok.as_view(), name='ok'),
+    path('db/create/', views.DBCreate.as_view(), name='db-create'),
 ]
