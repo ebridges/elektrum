0decf35746f34e7b9f52f3128c0ce5bc39f89303
Edward Bridges <github@eqbridges.com>
2019-08-30 05:05:28 -0400

    if the image already exists, then update it rather than fail

--- processor/src/main/java/cc/roja/photo/io/PhotoProcessorDAO.java
+++ processor/src/main/java/cc/roja/photo/io/PhotoProcessorDAO.java
@@ -55,8 +55,31 @@ public interface PhotoProcessorDAO {
       ":i.mimeType,\n" +
       ":i.shutterSpeed,\n" +
       ":i.shutterSpeedDenominator,\n" +
-      ":i.shutterSpeedNumerator\n" +
-   ")")
+      ":i.shutterSpeedNumerator) \n" +
+    "ON CONFLICT (id) DO UPDATE SET\n" +
+      "owner_id = EXCLUDED.owner_id,\n" + 
+      "aperture = EXCLUDED.aperture,\n" + 
+      "artist = EXCLUDED.artist,\n" + 
+      "camera_make = EXCLUDED.camera_make,\n" + 
+      "camera_model = EXCLUDED.camera_model,\n" + 
+      "create_date = EXCLUDED.create_date,\n" + 
+      "create_day_id = EXCLUDED.create_day_id,\n" + 
+      "file_path = EXCLUDED.file_path,\n" + 
+      "file_size = EXCLUDED.file_size,\n" + 
+      "focal_length_denominator = EXCLUDED.focal_length_denominator,\n" + 
+      "focal_length_numerator = EXCLUDED.focal_length_numerator,\n" + 
+      "gps_alt = EXCLUDED.gps_alt,\n" + 
+      "gps_date_time = EXCLUDED.gps_date_time,\n" + 
+      "gps_lat = EXCLUDED.gps_lat,\n" + 
+      "gps_lon = EXCLUDED.gps_lon,\n" + 
+      "image_height = EXCLUDED.image_height,\n" + 
+      "image_width = EXCLUDED.image_width,\n" + 
+      "iso_speed = EXCLUDED.iso_speed,\n" + 
+      "mime_type = EXCLUDED.mime_type,\n" + 
+      "shutter_speed = EXCLUDED.shutter_speed,\n" + 
+      "shutter_speed_denominator = EXCLUDED.shutter_speed_denominator,\n" + 
+      "shutter_speed_numerator = EXCLUDED.shutter_speed_numerator"
+ )
     // http://postgis.refractions.net/documentation/manual-1.5SVN/ST_MakePointM.html
   Integer insertImage(@BindBean("i") ImageInfo imageInfo);
 }
