293c07517a412d0d40fda2716853fd38c1667ad7
Edward Q. Bridges <ebridges@roja.cc>
2019-12-08 06:42:47 -0500

    switch to python-dotenv to avoid dependency conflict with lgw when using django-dotenv. also ran `poetry update`

--- project/elektrum/settings.py
+++ project/elektrum/settings.py
@@ -13,13 +13,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import sys
 from elektrum.env_util import locate_env_file, resolve_version
-import dotenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env_file = locate_env_file(BASE_DIR)
-dotenv.read_dotenv(env_file)
+load_dotenv(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
