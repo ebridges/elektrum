b293041eefb7b7f3095a3d5113342c22f5b2b558
Edward Bridges <github@eqbridges.com>
2020-03-23 06:39:46 -0400

    generate and configure a local pem file

--- tools/local-dev/https-server.py
+++ tools/local-dev/https-server.py
@@ -3,17 +3,20 @@ import ssl
 import os
 import sys
 
+# generate self signed certificate:
+#   `openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes`
+#
+# in chrome allow self-signed certs on localhost
+#   `chrome://flags/#allow-insecure-localhost`
+
+certfile = os.path.join(os.getcwd(), 'server.pem')
+
 if len(sys.argv) > 1:
-    web_dir = os.path.join(os.path.dirname(__file__), sys.argv[1])
+    web_dir = os.path.join(os.getcwd(), sys.argv[1])
     os.chdir(web_dir)
 
 httpd = HTTPServer(('localhost', 9443), SimpleHTTPRequestHandler)
 
-httpd.socket = ssl.wrap_socket(
-    httpd.socket,
-    keyfile='/Users/ebridges/Library/Caches/pypoetry/virtualenvs/elektrum-PlTQOJ1Z-py3.7/lib/python3.7/site-packages/sslserver/certs/development.key',
-    certfile='/Users/ebridges/Library/Caches/pypoetry/virtualenvs/elektrum-PlTQOJ1Z-py3.7/lib/python3.7/site-packages/sslserver/certs/development.crt',
-    server_side=True,
-)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile=certfile, server_side=True)
 
 httpd.serve_forever()
