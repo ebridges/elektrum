dafd2308aa922237b8a8536578b32e5d9f513bd1
Edward Bridges <github@eqbridges.com>
2020-05-09 21:21:59 -0400

    remove JS thats no longer needed and move necessary JS more local to place of usage

--- project/static/js/media_item.js
+++ project/static/js/media_item.js
@@ -1,21 +1,3 @@
-function renderThumbnails(images, locations) {
-  for (let loc of locations) {
-    let i = images.shift()
-    loadImage(
-      i.src,
-      function(img) {
-        img.style.width = i.w;
-        img.style.height = i.h;
-        img.style.maxWidth = '90vw';
-        loc.appendChild(img);
-      },
-      {
-        orientation: true,
-      }
-    );
-  }
-}
-
 function renderImage(image, loc) {
   loadImage(
     image.src,
