9fd21c819cb9cb7b17f297be47199fa761453f1e
Edward Bridges <github@eqbridges.com>
2020-05-12 12:23:56 -0400

    test using view and with a mock JPEG header

--- project/emailer/tests/test_init_mime_image.py
+++ project/emailer/tests/test_init_mime_image.py
@@ -5,12 +5,15 @@ from emailer.views.utils import init_mime_image
 
 @pytest.mark.django_db
 def test_init_mime_image(media_items):
+    JPEG_HEADER = b'\xff\xd8\xff\xe1-\xfcExif\x00\x00MM\x00*\x00\x00\x00\x08'
     for media_item in media_items:
-        init_mime_image(b'', media_item)
-        assert media_item['encoded'] is not None
-        image = media_item['encoded']
+        view = media_item.view()
+        init_mime_image(JPEG_HEADER, view)
+        assert view['encoded'] is not None
+        image = view['encoded']
+        assert image.get_content_type() == 'image/jpeg'
         assert image.get('Content-ID') is not None
-        assert image.get('Content-ID') == '<%s>' % media_item['item_id']
+        assert image.get('Content-ID') == '<%s>' % view['item_id']
         assert image.get('Content-Location') is not None
-        assert image.get('Content-Location') == media_item['media_item_url']
-        media_item['content_id'] == media_item['item_id']
+        assert image.get('Content-Location') == view['media_item_url']
+        view['content_id'] == view['item_id']
