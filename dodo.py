59fa2105bc88d34a18aa172259a598fa0b886944
Edward Bridges <github@eqbridges.com>
2020-06-06 12:13:41 -0400

    add task fo deploying lambda & setting up dns/gw api

--- dodo.py
+++ dodo.py
@@ -16,6 +16,7 @@ from elektrum.doit.task_actions import (
     environment,
     processor_archive,
     processor_version,
+    application_version,
 )
 
 
@@ -108,6 +109,45 @@ def task_build_application_service():
         'actions': [
             f'etc/bin/poetry2pip.py --file poetry.lock --output {requirements}',
             CmdAction('lgw lamdba-archive --verbose', env=args),
+        ],
+        'verbosity': 2,
+    }
+
+
+def task_deploy_application_service():
+    service = environ['SERVICE_NAME']
+    env = environment()
+    version = application_version()
+    archive_key = f'{service}-{env}-{version}.zip'
+    args = {
+        'PATH': environ['PATH'],
+        'AWS_ACCESS_KEY_ID': environ['AWS_ACCESS_KEY_ID'],
+        'AWS_SECRET_ACCESS_KEY': environ['AWS_SECRET_ACCESS_KEY'],
+        'AWS_LAMBDA_NAME': environ['APPLICATION_SERVICE_LAMBDA_NAME'],
+        'AWS_LAMBDA_DESCRIPTION': environ['APPLICATION_SERVICE_DESCRIPTION'],
+        'AWS_LAMBDA_HANDLER': environ['APPLICATION_SERVICE_LAMBDA_HANDLER'],
+        'AWS_LAMBDA_ARCHIVE_BUCKET': environ['APPLICATION_SERVICE_ARTIFACT_BUCKET_NAME'],
+        'AWS_LAMBDA_ARCHIVE_KEY': archive_key,
+        'AWS_LAMBDA_VPC_SUBNETS': environ['APPLICATION_SERVICE_SUBNET_IDS'],
+        'AWS_LAMBDA_VPC_SECURITY_GROUPS': environ['APPLICATION_SERVICE_SECURITY_GROUPS'],
+        'AWS_LAMBDA_EXECUTION_ROLE_ARN': environ['APPLICATION_SERVICE_ROLE_ARN'],
+        'AWS_API_LAMBDA_INTEGRATION_ROLE': environ['APPLICATION_SERVICE_LAMBDA_INTEGRATION_ROLE'],
+        'AWS_API_NAME': environ['APPLICATION_SERVICE_API_NAME'],
+        'AWS_API_DEPLOY_STAGE': environ['APPLICATION_SERVICE_API_DEPLOY_STAGE'],
+        'AWS_API_DOMAIN_NAME': environ['APPPICATION_SERVICE_API_DOMAIN_NAME'],
+        'AWS_ACM_CERTIFICATE_ARN': environ['APPLICATION_SERVICE_HTTPS_CERT_ARN'],
+        'AWS_LAMBDA_TAGS': environ['APPLICATION_SERVICE_TAGS'],
+        'AWS_LAMBDA_ENVIRONMENT': environ['APPLICATION_SERVICE_ENVIRONMENT'],
+        'AWS_LAMBDA_MEMORY_SIZE': environ['APPLICATION_SERVICE_MEMORY_SIZE'],
+        'AWS_LAMBDA_CONNECTION_TIMEOUT': environ['APPLICATION_SERVICE_TIMEOUT'],
+        'AWS_LAMBDA_RUNTIME': environ['APPLICATION_SERVICE_RUNTIME'],
+    }
+    return {
+        'file_dep': ['build/lambda-bundle.zip'],
+        'actions': [
+            CmdAction('lgw lambda-deploy -v --lambda-file=build/lambda-bundle.zip', env=args),
+            CmdAction('lgw gw-deploy --verbose', env=args),
+            CmdAction('lgw domain-add --verbose', env=args),
         ],
         'verbosity': 2,
     }
