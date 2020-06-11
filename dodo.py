15ede952e2316e38e7ed0e7834ce43e142f1c9a1
Edward Bridges <github@eqbridges.com>
2020-06-11 12:00:43 -0400

    move env declarations to a method to avoid them being evaluated during task initializing

--- dodo.py
+++ dodo.py
@@ -4,7 +4,6 @@ from sys import stdout, stderr
 from glob import glob
 
 from dotenv import load_dotenv
-from doit import create_after
 from doit.action import CmdAction
 
 from elektrum.doit.task_actions import (
@@ -59,27 +58,9 @@ def task_build_processor_service():
 def task_deploy_processor_service():
     """Deploys processor as an AWS lambda function"""
     i = ProcessorServiceInfo()
-    args = {
-        'PATH': environ['PATH'],
-        'AWS_ACCESS_KEY_ID': environ['AWS_ACCESS_KEY_ID'],
-        'AWS_SECRET_ACCESS_KEY': environ['AWS_SECRET_ACCESS_KEY'],
-        'AWS_LAMBDA_NAME': environ['MEDIA_PROCESSOR_LAMBDA_NAME'],
-        'AWS_LAMBDA_DESCRIPTION': environ['MEDIA_PROCESSOR_DESCRIPTION'],
-        'AWS_LAMBDA_HANDLER': environ['MEDIA_PROCESSOR_LAMBDA_HANDLER'],
-        'AWS_LAMBDA_ARCHIVE_BUCKET': environ['MEDIA_PROCESSOR_ARTIFACT_BUCKET_NAME'],
-        'AWS_LAMBDA_ARCHIVE_KEY': i.archive,
-        'AWS_LAMBDA_VPC_SUBNETS': environ['MEDIA_PROCESSOR_SUBNET_IDS'],
-        'AWS_LAMBDA_VPC_SECURITY_GROUPS': environ['MEDIA_PROCESSOR_SECURITY_GROUPS'],
-        'AWS_LAMBDA_TAGS': environ['MEDIA_PROCESSOR_TAGS'],
-        'AWS_LAMBDA_ENVIRONMENT': environ['MEDIA_PROCESSOR_ENVIRONMENT'],
-        'AWS_LAMBDA_EXECUTION_ROLE_ARN': environ['MEDIA_PROCESSOR_EXECUTION_ROLE_ARN'],
-        'AWS_LAMBDA_MEMORY_SIZE': environ['MEDIA_PROCESSOR_MEMORY_SIZE'],
-        'AWS_LAMBDA_CONNECTION_TIMEOUT': environ['MEDIA_PROCESSOR_TIMEOUT'],
-        'AWS_LAMBDA_RUNTIME': environ['MEDIA_PROCESSOR_RUNTIME'],
-    }
     return {
         'file_dep': i.deploy_deps(),
-        'actions': [CmdAction('lgw lambda-deploy --verbose', env=args)],
+        'actions': [CmdAction('lgw lambda-deploy --verbose', env=i.deploy_args)],
         'verbosity': VERBOSITY,
         'task_dep': ['build_processor_service'],
     }
@@ -97,84 +78,41 @@ def task_config_processor_service():
     }
 
 
-@create_after(executed='config')
 def task_build_application_service():
     i = ApplicationServiceInfo()
-
-    args = {
-        'PATH': environ['PATH'],
-        'AWS_LAMBDA_ARCHIVE_CONTEXT_DIR': '.',  # needs to be cwd so that etc/env is availabe in docker context
-        'AWS_LAMBDA_ARCHIVE_ADDL_FILES': f'{envfile()},$wkdir/.env;{i.versionfile},$wkdir;{i.requirements},$wkdir;{i.appdir}/,$wkdir',
-        'AWS_LAMBDA_ARCHIVE_ADDL_PACKAGES': 'postgresql,postgresql-devel',
-        'AWS_LAMBDA_ARCHIVE_BUNDLE_DIR': i.builddir,
-        'AWS_LAMBDA_ARCHIVE_BUNDLE_NAME': i.archive,
-    }
     return {
         'file_dep': i.file_deps(),
         'targets': [i.target],
         'actions': [
             f'etc/bin/poetry2pip.py --file poetry.lock --output {i.requirements}',
-            CmdAction('lgw lambda-archive --verbose', env=args),
+            CmdAction('lgw lambda-archive --verbose', env=i.build_args),
         ],
         'verbosity': VERBOSITY,
     }
 
 
-@create_after(executed='build_application_service')
 def task_deploy_application_service():
     i = ApplicationServiceInfo()
-    args = {
-        'PATH': environ['PATH'],
-        'AWS_ACCESS_KEY_ID': environ['AWS_ACCESS_KEY_ID'],
-        'AWS_SECRET_ACCESS_KEY': environ['AWS_SECRET_ACCESS_KEY'],
-        'AWS_LAMBDA_NAME': environ['APPLICATION_SERVICE_LAMBDA_NAME'],
-        'AWS_LAMBDA_DESCRIPTION': environ['APPLICATION_SERVICE_DESCRIPTION'],
-        'AWS_LAMBDA_HANDLER': environ['APPLICATION_SERVICE_LAMBDA_HANDLER'],
-        'AWS_LAMBDA_ARCHIVE_BUCKET': environ['APPLICATION_SERVICE_ARTIFACT_BUCKET_NAME'],
-        'AWS_LAMBDA_ARCHIVE_KEY': i.archive,
-        'AWS_LAMBDA_VPC_SUBNETS': environ['APPLICATION_SERVICE_SUBNET_IDS'],
-        'AWS_LAMBDA_VPC_SECURITY_GROUPS': environ['APPLICATION_SERVICE_SECURITY_GROUPS'],
-        'AWS_LAMBDA_EXECUTION_ROLE_ARN': environ['APPLICATION_SERVICE_EXECUTION_ROLE_ARN'],
-        'AWS_API_LAMBDA_INTEGRATION_ROLE': environ['APPLICATION_SERVICE_INTEGRATION_ROLE_ARN'],
-        'AWS_API_NAME': environ['APPLICATION_SERVICE_API_NAME'],
-        'AWS_API_DEPLOY_STAGE': environ['APPLICATION_SERVICE_API_DEPLOY_STAGE'],
-        'AWS_API_DOMAIN_NAME': environ['APPLICATION_SERVICE_API_DOMAIN_NAME'],
-        'AWS_ACM_CERTIFICATE_ARN': environ['APPLICATION_SERVICE_HTTPS_CERT_ARN'],
-        'AWS_LAMBDA_TAGS': environ['APPLICATION_SERVICE_TAGS'],
-        'AWS_LAMBDA_ENVIRONMENT': environ['APPLICATION_SERVICE_ENVIRONMENT'],
-        'AWS_LAMBDA_MEMORY_SIZE': environ['APPLICATION_SERVICE_MEMORY_SIZE'],
-        'AWS_LAMBDA_CONNECTION_TIMEOUT': environ['APPLICATION_SERVICE_TIMEOUT'],
-        'AWS_LAMBDA_RUNTIME': environ['APPLICATION_SERVICE_RUNTIME'],
-        'AWS_API_DESCRIPTION': environ['APPLICATION_SERVICE_API_DESCRIPTION'],
-    }
     return {
         'file_dep': [i.target, envfile()],
         'actions': [
-            CmdAction(f'lgw lambda-deploy --lambda-file={i.target}', env=args),
-            CmdAction('lgw gw-deploy --verbose', env=args),
-            CmdAction('lgw domain-add --verbose', env=args),
+            CmdAction(f'lgw lambda-deploy --lambda-file={i.target}', env=i.deploy_args),
+            CmdAction('lgw gw-deploy --verbose', env=i.deploy_args),
+            CmdAction('lgw domain-add --verbose', env=i.deploy_args),
         ],
         'verbosity': VERBOSITY,
         'task_dep': ['build_application_service'],
     }
 
 
-@create_after(executed='config')
 def task_build_thumbnail_service():
     i = ThumbnailServiceInfo()
-    args = {
-        'PATH': environ['PATH'],
-        'AWS_LAMBDA_ARCHIVE_BUNDLE_DIR': i.builddir,
-        'AWS_LAMBDA_ARCHIVE_BUNDLE_NAME': i.archive,
-        'AWS_LAMBDA_ARCHIVE_CONTEXT_DIR': f'{i.appdir}/',  # trailing slash significant for populating docker image
-        'AWS_LAMBDA_ARCHIVE_ADDL_FILES': 'src/,$wkdir;requirements.txt,$wkdir;version.txt,$wkdir',
-    }
     return {
         'file_dep': i.build_deps(),
         'targets': [i.target],
         'actions': [
             f'etc/bin/poetry2pip.py --file "{i.appdir}/poetry.lock" --output {i.requirements}',
-            CmdAction('lgw lambda-archive --verbose', env=args),
+            CmdAction('lgw lambda-archive --verbose', env=i.build_args),
         ],
         'verbosity': VERBOSITY,
     }
@@ -182,40 +120,12 @@ def task_build_thumbnail_service():
 
 def task_deploy_thumbnail_service():
     i = ThumbnailServiceInfo()
-    args = {
-        'PATH': environ['PATH'],
-        'AWS_REGION': environ['AWS_REGION'],
-        'AWS_ACCESS_KEY_ID': environ['AWS_ACCESS_KEY_ID'],
-        'AWS_SECRET_ACCESS_KEY': environ['AWS_SECRET_ACCESS_KEY'],
-        'AWS_LAMBDA_NAME': environ['THUMBNAIL_SERVICE_LAMBDA_NAME'],
-        'AWS_LAMBDA_DESCRIPTION': environ['THUMBNAIL_SERVICE_DESCRIPTION'],
-        'AWS_LAMBDA_HANDLER': environ['THUMBNAIL_SERVICE_LAMBDA_HANDLER'],
-        'AWS_LAMBDA_ARCHIVE_BUCKET': environ['THUMBNAIL_SERVICE_ARTIFACT_BUCKET_NAME'],
-        'AWS_LAMBDA_ARCHIVE_KEY': i.archive,
-        'AWS_LAMBDA_VPC_SUBNETS': environ['THUMBNAIL_SERVICE_SUBNET_IDS'],
-        'AWS_LAMBDA_VPC_SECURITY_GROUPS': environ['THUMBNAIL_SERVICE_SECURITY_GROUPS'],
-        'AWS_LAMBDA_EXECUTION_ROLE_ARN': environ['THUMBNAIL_SERVICE_EXECUTION_ROLE_ARN'],
-        'AWS_API_LAMBDA_INTEGRATION_ROLE': environ['THUMBNAIL_SERVICE_INTEGRATION_ROLE_ARN'],
-        'AWS_API_NAME': environ['THUMBNAIL_SERVICE_API_NAME'],
-        'AWS_API_DEPLOY_STAGE': environ['THUMBNAIL_SERVICE_API_DEPLOY_STAGE'],
-        'AWS_API_DOMAIN_NAME': environ['THUMBNAIL_SERVICE_API_DOMAIN_NAME'],
-        'AWS_ACM_CERTIFICATE_ARN': environ['THUMBNAIL_SERVICE_HTTPS_CERT_ARN'],
-        'AWS_LAMBDA_TAGS': environ['THUMBNAIL_SERVICE_TAGS'],
-        'AWS_LAMBDA_ENVIRONMENT': environ['THUMBNAIL_SERVICE_ENVIRONMENT'],
-        'AWS_LAMBDA_MEMORY_SIZE': environ['THUMBNAIL_SERVICE_MEMORY_SIZE'],
-        'AWS_LAMBDA_CONNECTION_TIMEOUT': environ['THUMBNAIL_SERVICE_TIMEOUT'],
-        'AWS_LAMBDA_RUNTIME': environ['THUMBNAIL_SERVICE_RUNTIME'],
-        'AWS_API_BINARY_TYPES': environ['THUMBNAIL_SERVICE_BINARY_TYPES'],
-        'AWS_API_RESPONSE_MODELS': environ['THUMBNAIL_SERVICE_RESPONSE_MODELS'],
-        'AWS_LAMBDA_ARCHIVE_BUNDLE_DIR': i.builddir,
-        'AWS_API_DESCRIPTION': environ['THUMBNAIL_SERVICE_API_DESCRIPTION'],
-    }
     return {
         'file_dep': i.deploy_deps(),
         'actions': [
-            CmdAction(f'lgw lambda-deploy --verbose --lambda-file={i.target}', env=args),
-            CmdAction('lgw gw-deploy --verbose', env=args),
-            CmdAction('lgw domain-add --verbose', env=args),
+            CmdAction(f'lgw lambda-deploy --verbose --lambda-file={i.target}', env=i.deploy_args),
+            CmdAction('lgw gw-deploy --verbose', env=i.deploy_args),
+            CmdAction('lgw domain-add --verbose', env=i.deploy_args),
         ],
         'verbosity': VERBOSITY,
         'task_dep': ['build_thumbnail_service'],
--- tools/elektrum/doit/task_actions.py
+++ tools/elektrum/doit/task_actions.py
@@ -54,6 +54,38 @@ class ApplicationServiceInfo(VersionInfo):
         self.requirements = f'{self.appdir}/requirements.txt'
         self.archive = f'{service()}-{environment()}-application-{self.version()}.zip'
         self.target = f'{self.builddir}/{self.archive}'
+        self.build_args = {
+            'PATH': environ['PATH'],
+            'AWS_LAMBDA_ARCHIVE_CONTEXT_DIR': '.',  # needs to be cwd so that etc/env is availabe in docker context
+            'AWS_LAMBDA_ARCHIVE_ADDL_FILES': f'{envfile()},$wkdir/.env;{self.versionfile},$wkdir;{self.requirements},$wkdir;{self.appdir}/,$wkdir',
+            'AWS_LAMBDA_ARCHIVE_ADDL_PACKAGES': 'postgresql,postgresql-devel',
+            'AWS_LAMBDA_ARCHIVE_BUNDLE_DIR': self.builddir,
+            'AWS_LAMBDA_ARCHIVE_BUNDLE_NAME': self.archive,
+        }
+        self.deploy_args = {
+            'PATH': environ['PATH'],
+            'AWS_ACCESS_KEY_ID': environ['AWS_ACCESS_KEY_ID'],
+            'AWS_SECRET_ACCESS_KEY': environ['AWS_SECRET_ACCESS_KEY'],
+            'AWS_LAMBDA_NAME': environ['APPLICATION_SERVICE_LAMBDA_NAME'],
+            'AWS_LAMBDA_DESCRIPTION': environ['APPLICATION_SERVICE_DESCRIPTION'],
+            'AWS_LAMBDA_HANDLER': environ['APPLICATION_SERVICE_LAMBDA_HANDLER'],
+            'AWS_LAMBDA_ARCHIVE_BUCKET': environ['APPLICATION_SERVICE_ARTIFACT_BUCKET_NAME'],
+            'AWS_LAMBDA_ARCHIVE_KEY': self.archive,
+            'AWS_LAMBDA_VPC_SUBNETS': environ['APPLICATION_SERVICE_SUBNET_IDS'],
+            'AWS_LAMBDA_VPC_SECURITY_GROUPS': environ['APPLICATION_SERVICE_SECURITY_GROUPS'],
+            'AWS_LAMBDA_EXECUTION_ROLE_ARN': environ['APPLICATION_SERVICE_EXECUTION_ROLE_ARN'],
+            'AWS_API_LAMBDA_INTEGRATION_ROLE': environ['APPLICATION_SERVICE_INTEGRATION_ROLE_ARN'],
+            'AWS_API_NAME': environ['APPLICATION_SERVICE_API_NAME'],
+            'AWS_API_DEPLOY_STAGE': environ['APPLICATION_SERVICE_API_DEPLOY_STAGE'],
+            'AWS_API_DOMAIN_NAME': environ['APPLICATION_SERVICE_API_DOMAIN_NAME'],
+            'AWS_ACM_CERTIFICATE_ARN': environ['APPLICATION_SERVICE_HTTPS_CERT_ARN'],
+            'AWS_LAMBDA_TAGS': environ['APPLICATION_SERVICE_TAGS'],
+            'AWS_LAMBDA_ENVIRONMENT': environ['APPLICATION_SERVICE_ENVIRONMENT'],
+            'AWS_LAMBDA_MEMORY_SIZE': environ['APPLICATION_SERVICE_MEMORY_SIZE'],
+            'AWS_LAMBDA_CONNECTION_TIMEOUT': environ['APPLICATION_SERVICE_TIMEOUT'],
+            'AWS_LAMBDA_RUNTIME': environ['APPLICATION_SERVICE_RUNTIME'],
+            'AWS_API_DESCRIPTION': environ['APPLICATION_SERVICE_API_DESCRIPTION'],
+        }
 
     def file_deps(self):
         deps = [f for f in glob(f'{self.appdir}/**', recursive=True) if isfile(f)]
@@ -72,6 +104,24 @@ class ProcessorServiceInfo(VersionInfo):
         self.versionfile = f'{self.appdir}/version.txt'
         self.archive = f'{service()}-{environment()}-processor-{self.version()}.zip'
         self.target = f'{self.appdir}/build/archives/{self.archive}'
+        self.deploy_args = {
+            'PATH': environ['PATH'],
+            'AWS_ACCESS_KEY_ID': environ['AWS_ACCESS_KEY_ID'],
+            'AWS_SECRET_ACCESS_KEY': environ['AWS_SECRET_ACCESS_KEY'],
+            'AWS_LAMBDA_NAME': environ['MEDIA_PROCESSOR_LAMBDA_NAME'],
+            'AWS_LAMBDA_DESCRIPTION': environ['MEDIA_PROCESSOR_DESCRIPTION'],
+            'AWS_LAMBDA_HANDLER': environ['MEDIA_PROCESSOR_LAMBDA_HANDLER'],
+            'AWS_LAMBDA_ARCHIVE_BUCKET': environ['MEDIA_PROCESSOR_ARTIFACT_BUCKET_NAME'],
+            'AWS_LAMBDA_ARCHIVE_KEY': self.archive,
+            'AWS_LAMBDA_VPC_SUBNETS': environ['MEDIA_PROCESSOR_SUBNET_IDS'],
+            'AWS_LAMBDA_VPC_SECURITY_GROUPS': environ['MEDIA_PROCESSOR_SECURITY_GROUPS'],
+            'AWS_LAMBDA_TAGS': environ['MEDIA_PROCESSOR_TAGS'],
+            'AWS_LAMBDA_ENVIRONMENT': environ['MEDIA_PROCESSOR_ENVIRONMENT'],
+            'AWS_LAMBDA_EXECUTION_ROLE_ARN': environ['MEDIA_PROCESSOR_EXECUTION_ROLE_ARN'],
+            'AWS_LAMBDA_MEMORY_SIZE': environ['MEDIA_PROCESSOR_MEMORY_SIZE'],
+            'AWS_LAMBDA_CONNECTION_TIMEOUT': environ['MEDIA_PROCESSOR_TIMEOUT'],
+            'AWS_LAMBDA_RUNTIME': environ['MEDIA_PROCESSOR_RUNTIME'],
+        }
 
     def build_deps(self):
         deps = [f for f in glob(f'{self.appdir}/src/main/**', recursive=True) if isfile(f)]
@@ -110,6 +160,41 @@ class ThumbnailServiceInfo(VersionInfo):
         self.requirements = f'{self.appdir}/requirements.txt'
         self.archive = f'{service()}-{environment()}-thumbnails-{self.version()}.zip'
         self.target = f'{self.builddir}/{self.archive}'
+        self.build_args = {
+            'PATH': environ['PATH'],
+            'AWS_LAMBDA_ARCHIVE_BUNDLE_DIR': self.builddir,
+            'AWS_LAMBDA_ARCHIVE_BUNDLE_NAME': self.archive,
+            'AWS_LAMBDA_ARCHIVE_CONTEXT_DIR': f'{self.appdir}/',  # trailing slash significant for populating docker image
+            'AWS_LAMBDA_ARCHIVE_ADDL_FILES': 'src/,$wkdir;requirements.txt,$wkdir;version.txt,$wkdir',
+        }
+        self.deploy_args = {
+            'PATH': environ['PATH'],
+            'AWS_REGION': environ['AWS_REGION'],
+            'AWS_ACCESS_KEY_ID': environ['AWS_ACCESS_KEY_ID'],
+            'AWS_SECRET_ACCESS_KEY': environ['AWS_SECRET_ACCESS_KEY'],
+            'AWS_LAMBDA_NAME': environ['THUMBNAIL_SERVICE_LAMBDA_NAME'],
+            'AWS_LAMBDA_DESCRIPTION': environ['THUMBNAIL_SERVICE_DESCRIPTION'],
+            'AWS_LAMBDA_HANDLER': environ['THUMBNAIL_SERVICE_LAMBDA_HANDLER'],
+            'AWS_LAMBDA_ARCHIVE_BUCKET': environ['THUMBNAIL_SERVICE_ARTIFACT_BUCKET_NAME'],
+            'AWS_LAMBDA_ARCHIVE_KEY': self.archive,
+            'AWS_LAMBDA_VPC_SUBNETS': environ['THUMBNAIL_SERVICE_SUBNET_IDS'],
+            'AWS_LAMBDA_VPC_SECURITY_GROUPS': environ['THUMBNAIL_SERVICE_SECURITY_GROUPS'],
+            'AWS_LAMBDA_EXECUTION_ROLE_ARN': environ['THUMBNAIL_SERVICE_EXECUTION_ROLE_ARN'],
+            'AWS_API_LAMBDA_INTEGRATION_ROLE': environ['THUMBNAIL_SERVICE_INTEGRATION_ROLE_ARN'],
+            'AWS_API_NAME': environ['THUMBNAIL_SERVICE_API_NAME'],
+            'AWS_API_DEPLOY_STAGE': environ['THUMBNAIL_SERVICE_API_DEPLOY_STAGE'],
+            'AWS_API_DOMAIN_NAME': environ['THUMBNAIL_SERVICE_API_DOMAIN_NAME'],
+            'AWS_ACM_CERTIFICATE_ARN': environ['THUMBNAIL_SERVICE_HTTPS_CERT_ARN'],
+            'AWS_LAMBDA_TAGS': environ['THUMBNAIL_SERVICE_TAGS'],
+            'AWS_LAMBDA_ENVIRONMENT': environ['THUMBNAIL_SERVICE_ENVIRONMENT'],
+            'AWS_LAMBDA_MEMORY_SIZE': environ['THUMBNAIL_SERVICE_MEMORY_SIZE'],
+            'AWS_LAMBDA_CONNECTION_TIMEOUT': environ['THUMBNAIL_SERVICE_TIMEOUT'],
+            'AWS_LAMBDA_RUNTIME': environ['THUMBNAIL_SERVICE_RUNTIME'],
+            'AWS_API_BINARY_TYPES': environ['THUMBNAIL_SERVICE_BINARY_TYPES'],
+            'AWS_API_RESPONSE_MODELS': environ['THUMBNAIL_SERVICE_RESPONSE_MODELS'],
+            'AWS_LAMBDA_ARCHIVE_BUNDLE_DIR': self.builddir,
+            'AWS_API_DESCRIPTION': environ['THUMBNAIL_SERVICE_API_DESCRIPTION'],
+        }
 
     def build_deps(self):
         deps = [f for f in glob(f'{self.appdir}/**', recursive=True) if isfile(f)]
