c76c3ae3d463632f8a1781258c170e748277aa05
Edward Bridges <github@eqbridges.com>
2020-07-06 17:36:37 -0400

    tweak testing lambda

--- tools/lambda/py/lambda.py
+++ tools/lambda/py/lambda.py
@@ -2,5 +2,6 @@ from botocore.vendored import requests
 
 
 def handler(event, context):
+    print('request began -- getting from https://reqres.in/api/users/2')
     response = requests.get('https://reqres.in/api/users/2')
     print(response.json())
