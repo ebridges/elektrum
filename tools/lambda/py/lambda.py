edb4db881b7e3cc744ada208511f284bd79fe368
Edward Bridges <github@eqbridges.com>
2020-08-06 08:32:54 -0400

    chore: switch to use urllib and jsonplaceholder

--- tools/lambda/py/lambda.py
+++ tools/lambda/py/lambda.py
@@ -1,7 +1,12 @@
-from botocore.vendored import requests
+from logging import debug, info, error
+from urllib.request import urlopen
 
 
 def handler(event, context):
-    print('request began -- getting from https://reqres.in/api/users/2')
-    response = requests.get('https://reqres.in/api/users/2')
-    print(response.json())
+    url = 'https://jsonplaceholder.typicode.com/todos/1'
+    r = urlopen(url)
+    if r.status != 200:
+        error(f'[ERROR] {url} returned {r.status}\n')
+        raise Exception(f'[ERROR] {url} returned {r.status}')
+    response = r.read().decode('utf-8')
+    info(response)
