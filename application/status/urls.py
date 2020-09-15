964def89b7cd18ba717c2340bb00a7df9bdca425
Edward Bridges <github@eqbridges.com>
2020-09-14 21:27:24 -0400

    refactor: migrate views into a separate set of modules.

--- application/status/urls.py
+++ application/status/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from status.views.ok import Ok
+from status.views.db_create import DBCreate
 
 urlpatterns = [
-    path('ok/', views.Ok.as_view(), name='ok'),
-    path('db/create/', views.DBCreate.as_view(), name='db-create'),
+    path('ok/', Ok.as_view(), name='ok'),
+    path('db/create/', DBCreate.as_view(), name='db-create'),
 ]
