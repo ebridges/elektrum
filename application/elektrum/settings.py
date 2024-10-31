c17dd956a70b3bc56fefb368c2b0340f19c2572f
Edward Bridges <github@eqbridges.com>
2024-10-31 19:29:32 -0400

    chore: linting fix

--- application/elektrum/settings.py
+++ application/elektrum/settings.py
@@ -18,7 +18,7 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from logging import info
 
-DEFAULT_SITE_ID=1
+DEFAULT_SITE_ID = 1
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
